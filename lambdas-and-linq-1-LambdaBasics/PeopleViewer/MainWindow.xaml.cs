0b00b7cdb02994205240c849e42aae65e4ee9247
ankitagarwl <ankitagarwl@hotmail.com>
2018-01-18 20:32:48 +0530

    ok

---
+++ lambdas-and-linq-1-LambdaBasics/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using PeopleViewer.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            Person selectedPerson = PersonListBox.SelectedItem as Person;
+
+            var repository = new PeopleRepository();
+            repository.GetPeopleCompleted +=
+                (repoSender, repoArgs) =>
+                {
+                    PersonListBox.ItemsSource = repoArgs.Result;
+                    if (selectedPerson != null)
+                        foreach (Person person in PersonListBox.Items)
+                            if (person.LastName == selectedPerson.LastName &&
+                                person.FirstName == selectedPerson.FirstName)
+                            {
+                                PersonListBox.SelectedItem = person;
+                            }
+                };
+            repository.GetPeopleAsync();
+            //selectedPerson = null;
+        }
+
+        //void repository_GetPeopleCompleted(object sender, GetPeopleCompletedEventArgs e)
+        //{
+        //    PersonListBox.ItemsSource = e.Result;
+        //}
+    }
+}
